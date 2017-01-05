79cd6f0b366c5256a7ba88bb06971888fe738d9f
vfridell <gusknows@gmail.com>
2017-01-05 15:15:39 -0500

    day11 simplified and generalized.  Can't believe this works.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,49 @@ namespace Advent_Of_Code_2016
     {
         static void Main(string[] args)
         {
-            Day11();
+            Day11Simple();
+        }
+
+        public static void Day11Simple()
+        {
+            int moveCount = 0;
+            int[] floorTest = {0, 5, 1, 4, 0};
+            int totalPieces = floorTest.Sum();
+            int elevatorPieces = Math.Min(floorTest[1], 2);
+            floorTest[1] -= elevatorPieces;
+            int currentFloor = 1;
+            while (floorTest[4] + 1 != totalPieces)
+            {
+                // go down
+                while (elevatorPieces < 2 && currentFloor > 1)
+                {
+                    currentFloor--;
+                    int piecesTaken = Math.Min(floorTest[currentFloor], 2 - elevatorPieces);
+                    if (piecesTaken > 0)
+                    {
+                        elevatorPieces += piecesTaken;
+                        floorTest[currentFloor] -= piecesTaken;
+                    }
+                    moveCount++;
+                }
+                // go up
+                while (currentFloor < 4)
+                {
+                    currentFloor++;
+                    int piecesTaken = Math.Min(floorTest[currentFloor], 2 - elevatorPieces);
+                    if (piecesTaken > 0)
+                    {
+                        elevatorPieces += piecesTaken;
+                        floorTest[currentFloor] -= piecesTaken;
+                    }
+                    moveCount++;
+                }
+
+                floorTest[4] += 1;
+                elevatorPieces--;
+            }
+
+            Console.WriteLine($"Minimum number of moves is {moveCount}");
         }
 
         public static void Day11()
